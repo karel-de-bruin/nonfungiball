6de018f6d76e611d402800854512a678a29f38ff
karel-de-bruin <79189708+karel-de-bruin@users.noreply.github.com>
2021-02-17 09:32:56 +0200

    Update players.js

--- functions/players.js
+++ functions/players.js
@@ -1,6 +1,7 @@
 exports.handler = async event => {
+  const id = myString.split("/").pop()
   return {
     statusCode: 200,
-    body: JSON.stringify({message: "Hello World", path: event.path})
+    body: JSON.stringify({message: "Hello World", id: id})
   }
 }
